f69398e786e6c6688eee9259f8cab4ac9d36d35a
July-4n <september4n@gmail.com>
2024-01-22 13:34:32 +0500

    feat: solve task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const way = path.join(__dirname, 'text.txt');
+
+const stream = fs.createReadStream(way, 'utf-8');
+
+stream.on('data', (chunk) => {
+  console.log(chunk);
+});
